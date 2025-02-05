1efa86f176470f690f4be651fd70e3e000d0a519
yehor <egor.karabanov@gmail.com>
2025-02-05 15:45:20 +0100

    user model modified, added CASCADE for profile and indexes

--- backend/app/database/models/user.py
+++ backend/app/database/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Boolean, select,or_
+from sqlalchemy import String, Boolean, select, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database.models.base import Base
@@ -10,15 +10,21 @@ if TYPE_CHECKING:
 
 class User(Base):
     # Corresponds to table "users"
-    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
-    username: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True, index=True)
+    username: Mapped[str] = mapped_column(String(255), nullable=False, unique=True, index=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
 
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
 
     # One-to-one relationship with Player.
-    player: Mapped["Player"] = relationship("Player", back_populates="user", uselist=False)
+    player: Mapped["Player"] = relationship(
+        "Player",
+        back_populates="user",
+        cascade="all, delete-orphan",
+        single_parent=True,
+        uselist=False
+    )
 
     @classmethod
     async def select_by_email_username(cls, session: AsyncSession, email: str, username: str):
